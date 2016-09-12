d14c10a40c1eea629140f080ef6e5de7b81ecc97
Rigil Malubay <rigil44@gmail.com>
2016-09-12 00:09:16 -0400

    Projectiles
    Added the logic of heated missle

---
+++ MainProject/Assets/Scripts/Launchers/MissileLauncher.cs
@@ -0,0 +1,48 @@
+﻿//
+// Script name: MissileLauncher
+//
+//
+// Programmer: Kentaurus
+//
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissileLauncher : Shooter.BasicLauncher
+{
+	#region Variables
+	#endregion
+
+	#region Unity API
+	[SerializeField] protected List<Transform> m_StartLocation = new List<Transform>();
+	[SerializeField] protected Transform m_Target;
+	#endregion
+
+	#region Public Methods
+	protected virtual void Start()
+	{
+		Fire(m_Target);
+	}
+	#endregion
+
+	#region Protected Methods
+	protected override void OnReloadCOmplete()
+	{
+		base.OnReloadCOmplete();
+		Fire(m_Target);
+	}
+
+	protected override void CreateProjectile(Transform target, Transform startlocation, Vector3 direction)
+	{
+		for (int i = 0; i < m_StartLocation.Count; ++i)
+		{
+			base.CreateProjectile(target, m_StartLocation[i], m_StartLocation[i].right);
+		}
+	}
+	#endregion
+
+	#region Private Methods
+	#endregion
+
+}
---
+++ MainProject/Assets/Scripts/Projectiles/Missle.cs
@@ -0,0 +1,66 @@
+﻿//
+// Script name: Missle
+//
+//
+// Programmer: Kentaurus
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class Missle : Shooter.BasicProjectile 
+{
+	#region Variables
+	[SerializeField] [Range(0f, 1f)] protected float m_LaunchingRatio = 0.1f;
+	[SerializeField] protected float m_MinVelocity = 0;
+	[SerializeField] protected float m_MaxSpeedIn = 1f;
+	protected float m_InitialVelociy = 0f;
+
+	private Timer m_RepositioningTimer = new Timer();
+	#endregion
+
+	#region Unity API
+	protected override void Update()
+	{
+		if (m_RepositioningTimer != null)
+		{
+			m_RepositioningTimer.OnUpdate();
+		}
+
+		CorrectingAngle();
+
+		base.Update();
+	}
+	#endregion
+
+	#region Public Methods
+	public override void Launch(Transform target, float velocity, Vector3 unityDirection)
+	{
+		m_RepositioningTimer.StartTimer(m_MaxSpeedIn);
+		m_InitialVelociy = velocity;
+		base.Launch(target, velocity, unityDirection);
+	}
+	#endregion
+
+	#region Protected Methods
+	protected override void Travel()
+	{
+		m_Velocity = Mathf.Clamp(m_InitialVelociy * m_RepositioningTimer.Ratio, m_MinVelocity, m_InitialVelociy);
+
+		base.Travel();
+	}
+
+	protected virtual void CorrectingAngle()
+	{
+		Vector3 wantedDirection = (m_Target.transform.position - transform.position).normalized;
+		Vector3 direction = transform.right;
+
+		m_UnityDirection += (wantedDirection - direction) * Mathf.Clamp01(m_RepositioningTimer.Ratio);
+		transform.right = m_UnityDirection;
+	}
+	#endregion
+
+	#region Private Methods
+	#endregion
+
+}
